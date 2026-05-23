522e2ee435e3ffa3824140dcd092487e205b51d4
Manuel CISSE <cisse.manuel@gmail.com>
2026-05-23 10:50:53 +0200

    Add icons in navigation bar

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,26 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MatIconModule],
   template: `
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">Recettes App</a>
+      <a class="navbar-brand" href="#"><mat-icon style="font-size: 22px; vertical-align: middle; margin-right: 0.4rem;">restaurant_menu</mat-icon>My Cook Book</a>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item">
-            <a class="nav-link" routerLink="/planning">Planning</a>
+            <a class="nav-link" routerLink="/planning"><mat-icon style="font-size: 18px; vertical-align: middle; margin-right: 0.25rem;">event</mat-icon>Planning</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" routerLink="/recipes">Recettes</a>
+            <a class="nav-link" routerLink="/recipes"><mat-icon style="font-size: 18px; vertical-align: middle; margin-right: 0.25rem;">restaurant</mat-icon>Recettes</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" routerLink="/shopping-list">Liste de courses</a>
+            <a class="nav-link" routerLink="/shopping-list"><mat-icon style="font-size: 18px; vertical-align: middle; margin-right: 0.25rem;">shopping_cart</mat-icon>Liste de courses</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" routerLink="/search">Recherche par ingrédient</a>
+            <a class="nav-link" routerLink="/search"><mat-icon style="font-size: 18px; vertical-align: middle; margin-right: 0.25rem;">search</mat-icon>Recherche par ingrédient</a>
           </li>
         </ul>
       </div>
